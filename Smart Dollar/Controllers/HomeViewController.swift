d691b09b90c7c0105ae7445f98818b150b3854a6
Pranshu Midha <pranshu1995@gmail.com>
2021-05-11 16:04:32 +0530

    Navigation controller for Home page

--- Smart Dollar/Controllers/HomeViewController.swift
+++ Smart Dollar/Controllers/HomeViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated);
+    self.navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+       
         // Do any additional setup after loading the view.
     }
-
+    
+   
 
 }
 
---
+++ Smart Dollar/Controllers/TransactionAddViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TransactionAddViewController.swift
+//  Smart Dollar
+//
+//  Created by Pranshu Midha on 10/05/21.
+//
+
+import UIKit
+
+class TransactionAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated);
+        self.navigationController?.isNavigationBarHidden = false;
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
