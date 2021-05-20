c224449287d8122a955e9eb6bb71d7603172dd7f
Prajna Bhat <prajna005@gmail.com>
2021-05-20 19:45:46 +1000

    Main settings page and currency convertor

--- Smart Dollar/Controllers/SettingsViewController.swift
+++ Smart Dollar/Controllers/SettingsViewController.swift
@@ -9,39 +9,130 @@ import UIKit
 import AppLocker
 
 
-class SettingsViewController: UIViewController {
 
-    var options = ALOptions();
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(sETTINGSTableViewCell.self, forCellReuseIdentifier: sETTINGSTableViewCell.identifier)
+        return table
+    }()
+    
+    var models = [Section]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lockerLoader();
-        // Do any additional setup after loading the view.
+        configure()
+        title = "Settings"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        
     }
+    
+    func configure() {
+        models.append(Section(title: "Settings", options: [
+            SettingsOption(title: "Name", icon: UIImage(named: "name.png"), iconBackgroundColor: .systemBlue) {
+        },
+        SettingsOption(title: "Currency", icon: UIImage(named: "currency.png"), iconBackgroundColor: .systemRed) {
+                            },
+        SettingsOption(title: "Security", icon: UIImage(named: "security.png"), iconBackgroundColor: .systemGreen) {
+                            }
+        ]))
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: sETTINGSTableViewCell.identifier, for: indexPath
+        ) as? sETTINGSTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        cell.configure(with: model)
+
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
+        
+        if model.title == "Name" {
+
+            
+        }
+        else if model.title == "Currency" {
+            let sendValue = ChangeCurrency();
+            sendValue.newcurrency()
+            
+            
+        }
+        else if model.title == "Security" {
+            
+        }
+        
+    }
+
+}
+
+
+
+
+  //  var options = ALOptions();
+    
+  //  override func viewDidLoad() {
+    //    super.viewDidLoad()
+    //    lockerLoader();
+        // Do any additional setup after loading the view.
+   // }
     
 //    var opts
-    @objc func lockerLoader(){
+ //   @objc func lockerLoader(){
     
-       options.image = UIImage(named: "lock")!;
+  //     options.image = UIImage(named: "lock")!;
     
-    options.title = "Smart Dollar Safe";
-    options.isSensorsEnabled = true;
+  //  options.title = "Smart Dollar Safe";
+  //  options.isSensorsEnabled = true;
        
-    options.onSuccessfulDismiss = { (mode: ALMode?) in
-        print("dekhba");
-           if let mode = mode {
-    print("Password \(String(describing: mode))d successfully");
-           } else {
-    print("User Cancelled");
-           }
-       }
-       options.onFailedAttempt = { (mode: ALMode?) in
-        print("naa dekhba");
-    print("Failed to \(String(describing: mode))");
-       }
+  //  options.onSuccessfulDismiss = { (mode: ALMode?) in
+   //     print("dekhba");
+  //         if let mode = mode {
+  //  print("Password \(String(describing: mode))d successfully");
+  //         } else {
+ //   print("User Cancelled");
+  //         }
+  //     }
+  //     options.onFailedAttempt = { (mode: ALMode?) in
+  //      print("naa dekhba");
+ //   print("Failed to \(String(describing: mode))");
+//       }
         
-        AppLocker.present(with: .create, and: options)
-    }
+ //       AppLocker.present(with: .create, and: options)
+ //   }
     
     
 
@@ -55,4 +146,4 @@ class SettingsViewController: UIViewController {
     }
     */
 
-}
+//}
